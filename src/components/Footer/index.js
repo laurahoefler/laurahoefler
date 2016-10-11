10c22068a2bc178c1b596f99b1ddcafd4c598ed2
Veit Lehmann <levito82@gmail.com>
2016-10-11 21:39:44 +0200

    dynamic navigation in header and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,39 @@
-import React from "react"
+import React, { PropTypes } from "react"
+import enhanceCollection from "phenomic/lib/enhance-collection"
 import { Link } from "phenomic/lib/Link"
 
 import styles from "./index.css"
 
-const Footer = () => (
-  <footer className={ styles.footer }>
-    { "Copyright © " }
-    { new Date().getFullYear() }
-    { " Laura Höfler " }
-    { " • " }
-    <Link
-      className={ styles.link }
-      to="/impressum/"
-    >
-      { "Impressum" }
-    </Link>
-  </footer>
-)
+const Footer = (props, { collection }) => {
+  const pages = enhanceCollection(collection, {
+    filter: {
+      layout: "Page",
+      nav: "footer",
+    },
+    sort: "order",
+  })
+
+  return (
+    <footer className={ styles.footer }>
+      { "Copyright © " }
+      { new Date().getFullYear() }
+      { " Laura Höfler " }
+      { pages.map((page, i) => [
+        " • ",
+        <Link
+          key={ `page-${ i }-${ page.__url }` }
+          className={ styles.link }
+          to={ page.__url }
+        >
+          { page.title }
+        </Link>,
+      ]) }
+    </footer>
+  )
+}
+
+Footer.contextTypes = {
+  collection: PropTypes.array.isRequired,
+}
 
 export default Footer
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,50 +1,51 @@
 import React, { PropTypes } from "react"
+import enhanceCollection from "phenomic/lib/enhance-collection"
 import Headroom from "react-headroom"
 import { Link } from "phenomic/lib/Link"
 
 import styles from "./index.css"
 
-const Header = () => (
-  <Headroom>
-    <header className={ styles.header }>
-      <nav className={ styles.nav }>
-        <div className={ styles.navPart1 }>
-          <Link
-            className={ styles.logo }
-            to="/"
-          >
-            { "Laura Höfler" }
-          </Link>
-        </div>
-        <div className={ styles.navPart2 }>
-          <Link
-            className={ styles.link }
-            activeClassName={ styles.linkActive }
-            to="/ueber-mich/"
-          >
-            { "Über mich" }
-          </Link>
-          <Link
-            className={ styles.link }
-            activeClassName={ styles.linkActive }
-            to="/referenzen/"
-          >
-            { "Referenzen" }
-          </Link>
-          <Link
-            className={ styles.link }
-            activeClassName={ styles.linkActive }
-            to="/kontakt/"
-          >
-            { "Kontakt" }
-          </Link>
-        </div>
-      </nav>
-    </header>
-  </Headroom>
-)
+const Header = (props, { collection }) => {
+  const pages = enhanceCollection(collection, {
+    filter: {
+      layout: "Page",
+      nav: "main",
+    },
+    sort: "order",
+  })
+
+  return (
+    <Headroom>
+      <header className={ styles.header }>
+        <nav className={ styles.nav }>
+          <div className={ styles.navPart1 }>
+            <Link
+              className={ styles.logo }
+              to="/"
+            >
+              { "Laura Höfler" }
+            </Link>
+          </div>
+          <div className={ styles.navPart2 }>
+            { pages.map((page, i) => (
+              <Link
+                key={ `page-${ i }-${ page.__url }` }
+                className={ styles.link }
+                activeClassName={ styles.linkActive }
+                to={ page.__url }
+              >
+                { page.title }
+              </Link>
+            )) }
+          </div>
+        </nav>
+      </header>
+    </Headroom>
+  )
+}
 
 Header.contextTypes = {
+  collection: PropTypes.array.isRequired,
   metadata: PropTypes.object.isRequired,
 }
 
